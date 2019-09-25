e6c98472763f2af22dcc13e29e53ebfe2dfd93d9
Yash Bharatula <ybharatu@purdue.edu>
2019-09-24 23:27:42 -0400

    Simple MongoProgram

---
+++ MongoClientInit.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+
+db = client['pymongo_test']
+
+posts = db.posts
+post_data = {
+    'title': 'Python and MongoDB',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Scott'
+}
+result = posts.insert_one(post_data)
+print('One post: {0}'.format(result.inserted_id))
+
+post_1 = {
+    'title': 'PyMongo!!!',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Scott'
+}
+post_2 = {
+    'title': 'Virtual Environments',
+    'content': 'Use virtual environments, you guys',
+    'author': 'Scott'
+}
+post_3 = {
+    'title': 'Learning Python',
+    'content': 'Learn Python, it is easy',
+    'author': 'Bill'
+}
+new_result = posts.insert_many([post_1, post_2, post_3])
+print('Multiple posts: {0}'.format(new_result.inserted_ids))
