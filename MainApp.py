5a1b3e194ec111cc3933f3dc5996fcb86fcf3260
Yash Bharatula <ybharatu@purdue.edu>
2019-09-25 22:16:41 -0400

    Can remove items

--- MainApp.py
+++ MainApp.py
@@ -1,6 +1,24 @@
 from pymongo import MongoClient
 
 
+# Function that removes an item from the database
+def remove_item( food, cmd ):
+    # Asks name of item to remove (TODO: Parse information from initial command and ask remaining info)
+    item_name = input("Name: ")
+
+    # Creates query to search
+    myquery = { "name" : item_name }
+
+    # Gets appropriate item
+    r_item = food.find_one({ "name" : item_name})
+
+    if r_item is not None:
+        # Removes item
+        food.delete_one( { "_id" : r_item.get("_id") })
+    else:
+        print("Item " + item_name + " is not in database")
+
+
 # Function that prints out all items
 def print_items( food, cmd ):
 
@@ -47,7 +65,12 @@ if __name__ == "__main__":
         # Parses command
         if cmd == "add":
             add_item(food, cmd)
-        if cmd == "print":
+        elif cmd == "print":
             print_items(food, cmd)
-        if cmd == "quit" or cmd == "exit":
+        elif cmd == "remove":
+            remove_item(food, cmd)
+        elif cmd == "quit" or cmd == "exit":
             break
+        else:
+            # TODO: Should have a -h or -verbose option
+            print("Not a valid command")
